ee71826cc42e0ec7c2cf0ec8d0764d4510d9280d
chandra2604 <chandraregina24@gmail.com>
2022-11-22 13:34:00 +0530

    Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NewExpense from './NewExpense';
+import Expenses from './Expenses';
 
 function App() {
+  const expenses=[
+    {
+      id:"e1",
+      title: "Paper",
+      amount:94.2,
+      date:new Date(2020,3,23),
+    },
+    {id:"e2",title:"Car Insurance", amount:99.65,date:new Date(2020,4,24)},
+    {id:"e3", title:"New Desk",amount:99.74,date:new Date(2020,5,25)},
+  ];
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Let's Get Started!</h1>
+      <NewExpense />
+      <Expenses items={expenses}/>
+      
+      </div>
   );
 }
 
---
+++ src/ExpenseItem.js
@@ -0,0 +1,23 @@
+import  React,{useState} from 'react';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+const ExpenseItem=(props)=>{
+    const [title,setTitle]= useState(props.title);
+    console.log('Expenseitem evaluated by React');
+    const clickHandler=()=>{
+   setTitle('Updated!!');
+    console.log(title);
+  };
+   return(
+        <div className="expense-item">  
+           <ExpenseDate date={props.date}/>
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
+            </div>
+            <button onClick={clickHandler}>Change Title</button>
+        </div>
+    );
+}
+export default ExpenseItem;
